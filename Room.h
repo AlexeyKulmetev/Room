05b7a60197dddabe0ab5d87addd0910a7209c7da
Aleksey_home_linux <pervoclassnic@gmail.com>
2024-09-07 15:21:30 +0300

    Added template of classes

--- Room.h
+++ Room.h
@@ -3,13 +3,13 @@
 
 #include <iostream>
 
-class TGoods {
+class Goods {
 protected:
     float _price;
     static float _extra;
 
 public:
-    TGoods(float price = 0) : _price{price} {}
+    Goods(float price = 0) : _price{price} {}
 
     void SetPrice(float p);
     
@@ -19,32 +19,32 @@ public:
 };
 
 
-class TRect {
+class Rect {
 protected:
     float _x1;
     float _x2;
 
 public:
-    TRect(float x1 = 0, float x2 = 0) : _x1{x1}, _x2{x2} {}
+    Rect(float x1 = 0, float x2 = 0) : _x1{x1}, _x2{x2} {}
 
     void SetSize(float x1, float x2);
 
     float Area() const;
 
-    TRect& operator = (const TRect&) = default; // explicitly allowed to create bitwise assignment
+    Rect& operator = (const Rect&) = default; // explicitly allowed to create bitwise assignment
 
-    friend std::ostream& operator << (std::ostream& out, const TRect& rect);
+    friend std::ostream& operator << (std::ostream& out, const Rect& rect);
 
-    friend std::istream& operator >> (std::istream in, TRect& rect);
+    friend std::istream& operator >> (std::istream in, Rect& rect);
 };
 
 
-class TBox: public TRect {
+class Box: public Rect {
 protected:
     float _x0;
 
 public:
-    TBox(float x0 = 0, float x1 = 0, float x2 = 0) : TRect(x1, x2), _x0{x0} {}
+    Box(float x0 = 0, float x1 = 0, float x2 = 0) : Rect(x1, x2), _x0{x0} {}
 
     void SetSize(float x0);
 
@@ -52,16 +52,47 @@ public:
 
     float Area() const; // surface area calculating method
 
-    TRect GetRect(int i, int j); // parallelepiped edge selection method
+    Rect GetRect(int i, int j); // parallelepiped edge selection method
 
-    TRect operator () (int i, int j); // parallelepiped edge selection operator
+    Rect operator () (int i, int j); // parallelepiped edge selection operator
 
-    friend std::ostream& operator << (std::ostream& out, const TBox& box);
+    friend std::ostream& operator << (std::ostream& out, const Box& box);
 
-    friend std::istream& operator >> (std::istream& in, TBox& box);
+    friend std::istream& operator >> (std::istream& in, Box& box);
 };
 
+    enum class TErr {OK, BUFFER_OVERFLOW, BAD_ALLOC};
 
+class Room : public Box, public Goods {
+public:
+    struct Rects: Rect {
+        int _count;
+        // Copy constructor "Rect(const Rect& other)"" will be implicitly created
+        Rects(const Rect& rect, int count) : Rect(rect), _count{count} {} 
+    };
+
+private:
+    Rect *_CarcassList{nullptr};
+    int  *_CarcassInd;
+    int MaxRect;
+    int CarcassNumber;
+    TErr ErrorState{TErr::OK};
+    // Element for links whin incorrectly accessed by index
+    static Rect BadRect;
+
+public:
+    Room(int maxrect = 10) : Box(), Goods() {
+        if (maxrect < 1) maxrect = 1;
+        MaxRect = maxrect;
+        _CarcassList = new Rect[MaxRect];
+        _CarcassInd = new int[MaxRect];
+    }
+
+    ~Room() {
+        if (_CarcassList) delete[] _CarcassList;
+        if (_CarcassInd) delete[] _CarcassInd;
+    }
+};
 
 
 #endif
